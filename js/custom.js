c9880defe5f1a04db05c729338822147ccc58f23
Alex <SmirkyGraphs@gmail.com>
2017-05-03 17:41:09 -0400

    Basic Site
    Start of website

---
+++ js/custom.js
@@ -0,0 +1,74 @@
+
+"use strict"; // Start of use strict
+
+
+function stickyHeader () {
+	if ($('.stricky').length) {
+		var strickyScrollPos = 100;
+		if($(window).scrollTop() > strickyScrollPos) {
+			$('.stricky').removeClass('fadeIn animated');
+	      	$('.stricky').addClass('stricky-fixed fadeInDown animated');
+	      	$('.scroll-to-top').fadeIn(500);
+		}
+		else if($(this).scrollTop() <= strickyScrollPos) {
+			$('.stricky').removeClass('stricky-fixed fadeInDown animated');
+	      	$('.stricky').addClass('slideIn animated');
+	      	$('.scroll-to-top').fadeOut(500);
+		}
+	};
+}
+
+// Fact Counter
+function factCounter() {
+	if($('.fact-counter').length){
+		$('.fact-counter .counter-column.animated').each(function() {
+
+			var $t = $(this),
+				n = $t.find(".count-text").attr("data-stop"),
+				r = parseInt($t.find(".count-text").attr("data-speed"), 10);
+
+			if (!$t.hasClass("counted")) {
+				$t.addClass("counted");
+				$({
+					countNum: $t.find(".count-text").text()
+				}).animate({
+					countNum: n
+				}, {
+					duration: r,
+					easing: "linear",
+					step: function() {
+						$t.find(".count-text").text(Math.floor(this.countNum));
+					},
+					complete: function() {
+						$t.find(".count-text").text(this.countNum);
+					}
+				});
+			}
+
+		});
+	}
+}
+
+
+
+// Elements Animation
+	if($('.wow').length){
+		var wow = new WOW(
+		  {
+			boxClass:     'wow',      // animated element css class (default is wow)
+			animateClass: 'animated', // animation css class (default is animated)
+			offset:       0,          // distance to the element when triggering the animation (default is 0)
+			mobile:       true,       // trigger animations on mobile devices (default is true)
+			live:         true       // act on asynchronously loaded content (default is true)
+		  }
+		);
+		wow.init();
+	}
+
+// instance of fuction while Window Scroll event
+jQuery(window).scroll(function () {
+	(function ($) {
+		stickyHeader();
+		factCounter();
+	})(jQuery);
+});
